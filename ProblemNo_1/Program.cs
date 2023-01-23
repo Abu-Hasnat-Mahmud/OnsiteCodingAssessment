8f9496eb5d3e8f37ef7dbe95bc556b71371dc20f
abuha <abuha@DESKTOP-DOSBFKK>
2023-01-23 12:52:22 +0600

    Program 1

--- ProblemNo_1/Program.cs
+++ ProblemNo_1/Program.cs
@@ -9,15 +9,19 @@ nums = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), Convert.ToInt32);
 Console.Write("Input val: ");
 val = Convert.ToInt32(Console.ReadLine());
 
-var count = -1;
+var count = 0;
 for (var i = 0; i < nums.Length - 1; i++)
 {
     if (nums[i] == val)
     {
         int temp = nums[i + 1];
+        if (temp!=val)
+        {
+            count++;
+        }
         nums[i] = temp;
         nums[i + 1] = val;
-        count++;
+       
     }
 }
 
