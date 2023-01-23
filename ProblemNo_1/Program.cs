a39f35e8b397468374f979713d42d6fadb2aba53
abuha <abuha@DESKTOP-DOSBFKK>
2023-01-23 12:57:38 +0600

    update assign value

--- ProblemNo_1/Program.cs
+++ ProblemNo_1/Program.cs
@@ -19,8 +19,8 @@ for (var i = 0; i < nums.Length - 1; i++)
         {
             count++;
         }
+        nums[i + 1] = nums[i];
         nums[i] = temp;
-        nums[i + 1] = val;
        
     }
 }
